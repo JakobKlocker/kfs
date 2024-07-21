af55c394d534940ce40d997db36b62786746d993
rperkmann <romanperkmann05@gmail.com>
2024-07-21 10:56:20 +0200

    small changes

--- src/ps2.zig
+++ src/ps2.zig
@@ -10,17 +10,17 @@ pub fn init() !PS2 {
 
 // this PS2 Dirver is referenced from https://wiki.osdev.org/%228042%22_PS/2_Controller
 const PS2 = struct {
-    const data: u16 = 0x60; // read and write data from/to the PS/2 Controller
-    const status: u16 = 0x64; // Status register contains flags for the data port
-    const command: u16 = 0x64; // Command register is used to send commands to the PS/2 controller
+    data: u16 = 0x60, // read and write data from/to the PS/2 Controller
+    status: u16 = 0x64, // Status register contains flags for the data port
+    command: u16 = 0x64, // Command register is used to send commands to the PS/2 controller
 
-    pub fn recv() u8 {
+    pub fn recv(this: PS2) u8 {
 
-        while (ports.inb(status) & 0b1 != 1) { // waiting for input; should be replaced with interrups later
+        while (ports.inb(this.status) & 0b1 != 1) { // waiting for input; should be replaced with interrups later
             ports.io_wait();
         }
 
-        const x = ports.inb(data);
+        const x = ports.inb(this.data);
         return x;
     }
 };
