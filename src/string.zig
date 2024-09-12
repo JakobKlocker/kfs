7e0f00b2d9f91e08c2d097e11978cef455502058
Jakob <jakobklocker@hotmail.com>
2024-09-12 17:52:29 +0200

    semi fix for strcmp, cmds work for now

--- src/string.zig
+++ src/string.zig
@@ -6,13 +6,13 @@ pub fn strlen(str: [*]u8) usize {
 
 //Find solution for this, can't compare .len because one could be 255 (like cmd) yet still smaller
 //can't compare null byte because it doesnt exist
-pub fn strcmp(s1: []const u8, s2: []const u8) bool {
+pub fn strcmp(s1: [*]const u8, s2: [*]const u8) bool {
     var i: usize = 0;
 
     // if (s1.len != s2.len)
     //     return false;
-    while (s1.len < i and s2.len < i) {
-        // while (s1[i] != 0 and s1[2] != 0) {
+    // while (s1.len < i and s2.len < i) {
+    while (s1[i] != 0 and s1[2] != 0) {
         if (s1[i] != s2[i])
             return false;
         i += 1;
