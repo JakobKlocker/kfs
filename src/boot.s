724ec186c7bc2e1aa6cde44ea9abbb85ed73d3e0
Jakob <jakobklocker@hotmail.com>
2024-09-17 21:26:07 +0200

    starting of paging, attempt from osdev kept crashing

--- src/boot.s
+++ src/boot.s
@@ -1,27 +1,37 @@
+.set MB_PAGE_ALIGN,  1 << 0
+.set MB_MEM_INFO,  1 << 1
+.set MB_USE_GFX,  1
+.set MB_MAGIC,  0x1BADB002
+.set MB_FLAGS,  MB_PAGE_ALIGN | MB_MEM_INFO | MB_USE_GFX
+.set MB_CHECKSUM, (0 - (MB_MAGIC + MB_FLAGS))
+
+
+
 .extern kernel_main
 
 .global _start
 .global stack_top
  
-
-.set MB_MAGIC, 0x1BADB002          // This is a 'magic' constant that GRUB will use to detect our kernel's location.
-.set MB_FLAGS, 0
-.set MB_CHECKSUM, (0 - (MB_MAGIC + MB_FLAGS))
  
 .section .multiboot
 	.align 4 
 	.long MB_MAGIC
 	.long MB_FLAGS
 	.long MB_CHECKSUM
+	; .long 0, 0, 0, 0, 0
+	; .long 0
+	; .long 1920
+	; .long 600
+	; .long 32
 
 .section .bss
 
 	.align 16
 	stack_bottom:
-		.skip 4096
+		.skip 16384
 	stack_top:
  
-.section .text
+.section .boot
 	_start:
 
 		mov $stack_top, %esp
@@ -32,3 +42,9 @@
 			cli      // Disable CPU interrupts
 			hlt      // Halt the CPU
 			jmp hang // If that didn't work, loop around and try again.
+
+.section .data
+.align 4096
+.global init_page_dir
+init_page_dir:
+	.quad 0x10000011b // Using 64 bit now since wont work with .long, in tut it said dd. Have to look into that
