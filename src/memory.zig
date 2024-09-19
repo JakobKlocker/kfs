6c63ef3d1ccfecd77d2e2d86cd80b610ebccc22b
Jakob <jakobklocker@hotmail.com>
2024-09-19 21:20:07 +0200

    PTE and PDE Flags setup

--- src/memory.zig
+++ src/memory.zig
@@ -1,20 +1,13 @@
-// const PAGE_PTE_FLAGS = enum  {
+//Virtual Address Format
+//AAAAAAAAAA         BBBBBBBBBB        CCCCCCCCCCCC
+// directory index    page table index  offset into page
 
-// 	PTE_PRESENT			=	1,		//0000000000000000000000000000001
-// 	PTE_WRITABLE		=	2,		//0000000000000000000000000000010
-// 	PTE_USER			=	4,		//0000000000000000000000000000100
-// 	PTE_WRITETHOUGH		=	8,		//0000000000000000000000000001000
-// 	PTE_NOT_CACHEABLE		=	0x10,		//0000000000000000000000000010000
-// 	PTE_ACCESSED		=	0x20,		//0000000000000000000000000100000
-// 	PTE_DIRTY			=	0x40,		//0000000000000000000000001000000
-// 	PTE_PAT			=	0x80,		//0000000000000000000000010000000
-// 	PTE_CPU_GLOBAL		=	0x100,		//0000000000000000000000100000000
-// 	PTE_LV4_GLOBAL		=	0x200,		//0000000000000000000001000000000
-//    	PTE_FRAME			=	0x7FFFF000 	//1111111111111111111000000000000
-// };
+const PAGES_SIZE = 4096;
+const PAGES_PER_TABLE = 1024;
+const TABLES_PER_DIR = 1024;
 
 //https://wiki.osdev.org/images/6/60/Page_table_entry.png
-const PAGE_PTE_FLAGS = packed struct {
+const PAGE_PTE_FLAGS = packed struct(u32) {
     PTE_PRESENT: u1 = 0, // 1 bit for Present (P)
     PTE_WRITABLE: u1 = 0, // 1 bit for Read/Write (RW)
     PTE_USER: u1 = 0, // 1 bit for User/Supervisor (U/S)
@@ -24,7 +17,21 @@ const PAGE_PTE_FLAGS = packed struct {
     PTE_DIRTY: u1 = 0, // 1 bit for Dirty (D)
     PTE_PAT: u1 = 0, // 1 bit for Page Attribute Table (PAT)
     PTE_CPU_GLOBAL: u1 = 0, // 1 bit for Global Page (G)
-    PTE_AVL: u3 = 0, // 3 bits (AVL)
+    PTE_AVL: u3 = 0, // 3 bits (AVL/UNUSED)
+    PTE_FRAME: u20 = 0, // 20 bits for the frame address (physical page)
+};
+
+//Page Directory Entry
+const PAGE_PDE_FLAGS = packed struct(u32) {
+    PTE_PRESENT: u1 = 0, // 1 bit for Present (P)
+    PTE_WRITABLE: u1 = 0, // 1 bit for Read/Write (RW)
+    PTE_USER: u1 = 0, // 1 bit for User/Supervisor (U/S)
+    PTE_WRITETHROUGH: u1 = 0, // 1 bit for Write-through (PWT)
+    PTE_NOT_CACHEABLE: u1 = 0, // 1 bit for Cache Disable (PCD)
+    PTE_ACCESSED: u1 = 0, // 1 bit for Accessed (A)
+    PTE_UNUSED: u1 = 0, // 1 bit
+    PTE_PAT: u1 = 0, // 1 bit for Page Attribute Table (PAT)
+    PTE_UNUSED1: u4 = 0, // 3 bits (AVL/UNUSED)
     PTE_FRAME: u20 = 0, // 20 bits for the frame address (physical page)
 };
 
